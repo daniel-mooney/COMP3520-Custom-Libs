51bccce7acddb1fac1c166b7635eb4c0305609c9
Daniel Mooney <daniel.mooney180@gmail.com>
2024-10-07 11:45:07 +1100

    temp test dequeu

--- deque_test.c
+++ deque_test.c
@@ -9,12 +9,14 @@ int main() {
 
     printf("Length: %zu\n", deque_length(&deque));
     printf("Item size: %zu\n", deque_item_size(&deque));
+    printf("Empty: %d\n", deque_empty(&deque));
 
     int item;
     deque_pop_front(&deque, &item);
 
     printf("Popped item: %d\n", item);
 
+    item = 6;
     deque_push_front(&deque, &item);
 
     deque_front(&deque, &item);
@@ -22,6 +24,16 @@ int main() {
 
     deque_back(&deque, &item);
     printf("Back: %d\n", item);
+
+    printf("Length: %zu\n", deque_length(&deque));
+
+    for (int i = 0; deque_length(&deque) > 0; i++) {
+        deque_pop_front(&deque, &item);
+        printf("Popped item: %d\n", item);
+    }
+
+    printf("Length: %zu\n", deque_length(&deque));
+    printf("Empty: %d\n", deque_empty(&deque));
 
     deque_destroy(&deque);
     return 0;
